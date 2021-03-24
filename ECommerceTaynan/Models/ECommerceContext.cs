47429b9251e5d29c1c8d95c62a851afb6ac04307
Taynan <taynan125silva@gmail.com>
2021-03-24 19:37:08 -0300

    Criando a classe DbContext

---
+++ ECommerceTaynan/Models/ECommerceContext.cs
@@ -0,0 +1,12 @@
+﻿using System.Data.Entity;
+
+namespace ECommerceTaynan.Models
+{
+    public class ECommerceContext : DbContext
+    {
+        public ECommerceContext() : base("DefaultConnection")
+        {
+
+        }
+    }
+}
